6631c454e3deddfe7a38013787f0d26627a2ed5f
brody <49502879+meloknight@users.noreply.github.com>
2024-06-03 14:47:21 -0700

    Add log to track adjustedAngleConstant for higher refresh screens.

--- src/utils/loopFunctions.ts
+++ src/utils/loopFunctions.ts
@@ -21,6 +21,8 @@ export function updateShipDirection(
   const normalizedDeltaTo60FPS = deltaFPS / 60;
   const adjustedAngleConstant = SHIP_ANGLE_CONSTANT / normalizedDeltaTo60FPS;
 
+  console.log(adjustedAngleConstant);
+
   if (shipActionFlagsRef.current.directionRightIsActive) {
     if (shipParams.current.shipAngle + adjustedAngleConstant >= 2 * Math.PI) {
       shipParams.current.shipAngle = 0;
